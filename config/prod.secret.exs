817b9ba50850a09c664e7907f988ca213f44f1cc
hongjie221 <1091840333@qq.com>
2019-11-01 22:11:54 -0400

    add deploy file

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -27,6 +27,9 @@ config :timesheet, TimesheetWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   secret_key_base: secret_key_base
 
+
+config :timesheet, TimesheetWeb.Endpoint, server: true
+
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
